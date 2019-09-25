2488ad2e23e9e7b89b62663460dc8c8c9a374046
Olav Valle <olavval@stud.ntnu.no>
2019-09-25 13:24:41 +0200

    add work done on tech-support1 chapter 6

---
+++ projects/personal/MultiplesOfFive.java
@@ -0,0 +1,15 @@
+public class MultiplesOfFive
+{
+	public static void main(String[] args)
+	{
+		int index = 2;
+		
+		while(index < 20){
+			System.out.println(5*index);
+			index++;
+		}
+	} 
+	
+	
+}
+
